4e21cc730765686792edc8e1fb0422db0cd0535f
superzhao <superlizhao@163.com>
2018-09-20 15:51:34 +0800

    detail list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,17 +2,68 @@
   <div>
     <detail-banner/>
     <detail-header/>
+    <detail-list :list="list"/>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
+  data () {
+    return {
+      list: [
+        {
+          title: '中秋活动票',
+          children: [
+            {
+              title: '大学生票'
+            },
+            {
+              title: '[夜场]成人票'
+            },
+            {
+              title: '三馆联票',
+              children: [
+                {
+                  title: '三馆联票-馆一'
+                },
+                {
+                  title: '三馆联票-馆二'
+                },
+                {
+                  title: '三馆联票-馆三'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          title: '全价票'
+        },
+        {
+          title: '优惠票',
+          children: [
+            {
+              title: '儿童票'
+            },
+            {
+              title: '老人票'
+            }
+          ]
+        },
+        {
+          title: '一日游'
+        }
+      ]
+    }
+  },
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
   }
 }
 </script>
