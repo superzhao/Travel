647af65327671499dc3f11c0717f758b88904f00
superzhao <superlizhao@163.com>
2018-09-20 14:56:21 +0800

    fix problem

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -30,10 +30,13 @@ export default {
   },
   activated () {
     window.addEventListener('scroll', this.handleScroll)
-    // window.addEventListener('scroll', this.handleScroll)
+  },
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
   },
   methods: {
     handleScroll () {
+      console.log('scroll')
       const top = document.documentElement.scrollTop
       if (top > 60) {
         let opacity = top / 140
